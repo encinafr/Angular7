f09d284749852fca96fece14950518226b75eadc
Encina Javier <encinajavier7@gmail.com>
2019-03-29 19:21:28 -0300

    Agregando al repositorio remoto

---
+++ CursoAngular7/Angular.API/Angular.API/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Angular.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok(new
+            {
+                Nombre = "Pedro",
+                Apellido = "Perez",
+                Email = "prueba@angular.com"
+            });
+        }
+
+        // GET api/values/5
+        [HttpGet("users")]
+        public ActionResult GetUsers()
+        {
+            List<UserViewModel> users = new List<UserViewModel>();
+
+            users.Add(new UserViewModel
+            {
+                LastName = "Garcia",
+                Name = "Beto",
+                Email = "email@email.com"
+            });
+
+            users.Add(new UserViewModel
+            {
+                LastName = "Alonzo",
+                Name = "Beto",
+                Email = "email@email.com"
+            });
+
+            users.Add(new UserViewModel
+            {
+                LastName = "Puente",
+                Name = "Tito",
+                Email = "email@email.com"
+            });
+
+            return Ok(users);
+        }
+
+        // GET api/values/5
+        [HttpPost("adduser")]
+        public ActionResult AddUser([FromBody]UserViewModel user)
+        {
+            return Ok();
+        }
+
+    }
+}
